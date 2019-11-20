a408dcc36d341d97a844f555ad761391de70676f
paul santamaria <pauliix.santamaria@gmail.com>
2019-11-20 22:14:01 +0100

    sans tours

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 let $modal = document.getElementById("Modal");
 let $open = document.getElementById("openModal");
 let $close = document.getElementsByClassName("closeModal")[0];
+let $endgame = document.getElementById("gameOver");
 
-let $all = document.querySelector('.exter');
-let $green = document.querySelector('.vert');
-let $red = document.querySelector('.rouge');
-let $blue = document.querySelector('.blue');
-let $yellow = document.querySelector('.yellow');
+ const $green = document.querySelector('#vert');
+ const $red = document.querySelector('#rouge');
+ const $blue = document.querySelector('#bleu');
+ const $yellow = document.querySelector('#jaune');
+ const $all = [$red, $blue, $green, $yellow];
+
+let $RandomChain = [];
+let $UserChain = [];
+let $steps = 0;
+let $erreur = false;
 
 $scoreRound = document.getElementById('scoreByRound');
 $scoreClick = document.getElementById('scoreByClick');
+$scoreClick = 0;
+$scoreRound = 0;
 
 
 //////   MODAL   /////
@@ -19,18 +27,74 @@ $open.onclick = function() {
 ///// FERMETURE MODAL /////
 $close.onclick = function() {
     $modal.style.display = "none";
+    $endgame.style.display = "none";
+
 }
 window.onclick = function(event) {
-if (event.target == $modal) {
-    $modal.style.display = "none";
-}
-}
-
-function regles() {
-    alert("Bienvenue sur mon Jeu Super Simon ! <br>Pour lancer le jeu, cliquez sur 'Jouer'");
+    if (event.target == $modal) {
+        $modal.style.display = "none";
+        $endgame.style.display = "none";
+    }
 }
 
 
+function initialisation() {
+    setTimeout(() => displayChain(), 300);
+}
+
+function ajout() {
+    for (let i = 0; i < $RandomChain.length; i++) {
+       (function(j) {
+            setTimeout(function timer() {
+                        effet($RandomChain[j]);
+                        if(j == $RandomChain.length - 1) { 
+                            $RandomChain.push(effet);
+                            setTimeout(() => {
+                                effet($RandomChain[j+1]);
+                            }, 1000);
+                        }
+            }, 1000 * j);
+        })(i);
+   }
+}
+
+const displayChain = () => {
+    const $randomChoice = $all[Math.floor(Math.random() * 4)];
+    if($RandomChain.length >= 1) {  
+         ajout();
+    }
+    else {  
+        const $idrandomChoice = $randomChoice.id;
+        $RandomChain.push($idrandomChoice); 
+         effet($randomChoice); 
+         
+    }
+};
+
+function effet ($randomChoice) {
+	$randomChoice.classList.add("effet");
+	setTimeout(() => {
+		$randomChoice.classList.remove("effet");;
+	}, 500);
+}
+
+const $userClick = (e) => {
+    $UserChain.push(e.target.id);
+    if($UserChain[$steps] === $RandomChain[$steps]) {
+        console.log('yes');
+        $steps++;
+        $erreur=false; 
+    }
+    else{   
+        endgame()
+    }
+}
 function start(){
-    console.log('Jeu commencé')
+    initialisation();
 }
+function endgame () {
+    $endgame.style.display = "flex";
+}
+
+document.getElementById('play').addEventListener("click", $userClick);
+
